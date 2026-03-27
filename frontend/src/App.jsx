8b4dc23a89cec75836f4d5afd77b08aeea81fb21
Minseong <akaza@Minseongui-MacBookAir.local>
2026-03-27 11:44:00 +0900

    reactのviteがコード変更を感知するよう設定

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,2 +1,26 @@
 import { useState } from 'react';
-import './App.css'
+import Login from './pages/Login';
+
+function App() {
+    // 現在ログインしている講師ID
+    const [loggedInTeacherId, setLoggedInTeacherId] = useState(null);
+
+    const handleLoginSuccess = (teacerId) => {
+        setLoggedInTeacherId(teacerId);
+    }
+
+    return(
+        <div>
+            {!loggedInTeacherId ? (<Login onLoginSucess={handleLoginSuccess}/>
+            ) : (
+                <div style={{ padding: '20px', textAlign: 'center', marginTop: '50px'}}>
+                    <h1>ログイン成功</h1>
+                    <p>ようこそ, {loggedInTeacherId}先生</p>
+                    {/*To do: dashboardコンポナント */}
+                </div> 
+            )}
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import axios from 'axios'
+import './Login.css'
+
+export default function Login ({ onLoginSuccess }) {
+    const [loginId, setLoginId] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoading, setIsLoding] = useState(false);
+
+    // ログインボタンのアクション
+    const handleLogin = async (e) => {
+        e.preventDefault(); // 画面の無駄なリロードを防ぐ
+        setErrorMessage('');
+
+        // 入力チェック
+        if (!loginId || !password) {
+            setErrorMessage('IDとパスワードを入力してください。');
+            return;
+        }
+
+        setIsLoding(true);
+
+        try {
+            // TODO: 実際のバックエンドAPIにRequestを送る
+            console.log('API通信データ：', {login_id: loginId,password});
+
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            // test 
+            if (loginId.startWith('teacher_')) {
+                onLoginSuccess(loginId); // 成功をApp.jsxに通知
+            } else {
+                setErrorMessage('IDまたはパスワードが間違っています。')
+            }
+        } catch (error) {
+            setErrorMessage('サーバとの通信に失敗しました。');
+        } finally {
+            setIsLoding(false);
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h2 className="login-title">講師ログイン</h2>
+                <p className="login-subtitle">成績表送信システムへようこそ</p>
+
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+                <form onSubmit={handleLogin}>
+                    <div className="input-group">
+                        <label>ログイン ID</label>
+                        <input
+                            type="text"
+                            placeholder="idを入力"
+                            value={loginId}
+                            onChange={(e) => setLoginId(e.target.value)}
+                            disabled={isLoading}
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <label>パスワード</label>
+                        <input
+                            type="password"
+                            placeholder="パスワードを入力"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            disabled={isLoading}
+                        />
+                    </div>
+
+                    <button type="submit" className="login-button" disabled={isLoading}>
+                        {isLoading ? '確認中...':'ログイン'} 
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
